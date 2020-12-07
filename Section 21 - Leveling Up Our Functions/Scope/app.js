e9c68d3f5933bd2c386d54fdd65d5b5350728524
JustPrix <prixstormy@gmail.com>
2020-12-07 07:44:21 +0530

    Update section 21

---
+++ Section 21 - Leveling Up Our Functions/Scope/app.js
@@ -0,0 +1,39 @@
+// let totalEggs = 0
+// function collectEggs(){
+//   totalEggs = 6;
+// }
+// console.log(totalEggs);
+// collectEggs();
+// console.log(collectEggs);
+
+// let bird = 'Scarlet Macaw';
+// function birdWatch() {
+//   let bird = 'Great Blue Heron';
+//   console.log(bird);
+// }
+// birdWatch();
+
+// let radius = 8;
+// if (radius > 0){
+//   const PI = 3.14159;
+//   let msg = 'Hi';
+// }
+
+// console.log(radius);
+// console.log(msg)
+
+// for (let i = 0; i < 5; i++){
+//   let msg = 'Hello';
+// }
+// console.log(msg)
+// console.log(i)
+
+// function bankRobbery() {
+//   const heroes = ['Spiderman', 'Wolverine', 'Black Panther', 'Wonder Woman'];
+//   function cryForHelp() {
+//     for (let hero of heroes) {
+//       console.log(`PLEASE HELP US, ${hero.toUpperCase}`);
+//     }
+//   }
+//   cryForHelp();
+// }
