f67a26eafc1a206b8bc0d239c05dc0013133a963
JustPrix <prixstormy@gmail.com>
2020-12-17 01:32:52 +0530

    Updated section 29 and readme

--- Section 29 - Prototype, Classes and OOP/OOP/app.js
+++ Section 29 - Prototype, Classes and OOP/OOP/app.js
@@ -1,7 +1,38 @@
-String.prototype.yell = function () {
-  return `OMG!!! ${this.toUpperCase()}!!!!!`;
-};
+// String.prototype.yell = function () {
+//   return `OMG!!! ${this.toUpperCase()}!!!!!`;
+// };
 
-Array.prototype.pop = function () {
-  return 'SORRY I WANT THAT ELEMENT, I WILL NEVER POP IT OFF!';
-};
+// Array.prototype.pop = function () {
+//   return 'SORRY I WANT THAT ELEMENT, I WILL NEVER POP IT OFF!';
+// };
+
+//=====>> Extends and Super
+
+class Pet {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  eat() {
+    return `${this.name} is eating!`;
+  }
+}
+
+class Cat extends Pet {
+  constructor(name, age, livesLeft = 9) {
+    super(name, age);
+    this.livesLeft = livesLeft;
+  }
+  meow() {
+    return 'MEOOWWWW!!';
+  }
+}
+
+class Dog extends Pet {
+  bark() {
+    return 'WOOFF!!';
+  }
+  eat() {
+    return `${this.name} scarfs his food!`;
+  }
+}
